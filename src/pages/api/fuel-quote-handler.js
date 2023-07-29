952ca20df99e83876840a86e1e3bbaed2798fef1
bahnaynays <60674300+bahnaynays@users.noreply.github.com>
2023-07-28 20:15:22 -0500

    change 1
    added to fuel-quote-handler

--- src/pages/api/fuel-quote-handler.js
+++ src/pages/api/fuel-quote-handler.js
@@ -1,7 +1,14 @@
 import { getDatabase } from "./db";
-import { getSession } from 'next-auth/clinet'
+import { getSession } from 'next-auth/client'
 
 export default async function handler(req,res){
+
+    //check for user session
+    const session = await getSession({ req });
+    if(!session){
+        return res.status(401).json({ error: 'No session exists' });
+    }
+
     if(req.method !== 'POST'){
         return res.status(405).end();
     }
